1a2ed7e5fc111ce6c74da256e81528323f7c0476
Laura M <lamorenoca@unal.edu.co>
2023-11-13 17:58:20 +0100

    kWh function added

--- Exercise4/src/Kitchen.java
+++ Exercise4/src/Kitchen.java
@@ -1,12 +1,23 @@
+import java.lang.Math;
+
 public class Kitchen extends Room {
     boolean Stove; // Attribute representing the stove status
+    long kWh;
 
     public Kitchen (String name, boolean light, boolean Stove){ // Constructor of the class that considers the inheritance of the class Room
         super(name,light);
         this.Stove = Stove;
+        this.kWh=0;
     }
     public void switchStove(){ // Method that modifies the state of the Stove
+
         Stove =!Stove;
+        if (Stove){
+            kWh=System.currentTimeMillis();
+        } else{
+            kWh=(long)((System.currentTimeMillis()-kWh)*2000/(Math.pow(3.6,10^6)));
+            System.out.println(kWh+" kilowatt-hours were consumed by the stove");
+        }
     }
     public boolean energyWarning() { //Method that adds a message to the user when the stove is on
         super.energyWarning();
